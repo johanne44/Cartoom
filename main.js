4c894cf2724165b2bc305df16d2bf4f27d2a4283
johanne44 <92949975+johanne44@users.noreply.github.com>
2023-08-14 18:27:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+      function changeColor(element) {
+         var currentColor = element.style.backgroundColor;
+         if(currentColor == "skyblue") {
+            element.style.backgroundColor = "navy";
+         } else {
+            element.style.backgroundColor = "skyblue"; 
+         }
+      }
